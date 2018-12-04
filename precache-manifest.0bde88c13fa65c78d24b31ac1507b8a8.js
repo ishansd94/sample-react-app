030c0d285d68b939f2c40b7630eb719f72780089
Ishan Dassanayake <ishansd94@gmail.com>
2018-12-04 15:33:26 +0530

    Updates

---
+++ precache-manifest.0bde88c13fa65c78d24b31ac1507b8a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f20619754a31b678f8ff",
+    "url": "/sample-react-app/static/js/runtime~main.f2061975.js"
+  },
+  {
+    "revision": "aee6e84ca8e4def6f978",
+    "url": "/sample-react-app/static/js/main.aee6e84c.chunk.js"
+  },
+  {
+    "revision": "2818dbc80ba3c939eefa",
+    "url": "/sample-react-app/static/js/1.2818dbc8.chunk.js"
+  },
+  {
+    "revision": "aee6e84ca8e4def6f978",
+    "url": "/sample-react-app/static/css/main.bbbeddb7.chunk.css"
+  },
+  {
+    "revision": "7b94faaa503bb2c75ecce664df112ee5",
+    "url": "/sample-react-app/index.html"
+  }
+];
